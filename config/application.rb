2b2a0ddd399d2bed0b13ad23dfaa8615cdb1606f
cyb- <gchateau@student.42.fr>
2017-01-19 02:20:48 +0100

    default language to french with translations

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Paydev
   class Application < Rails::Application
     config.i18n.load_path += Dir[File.join(Rails.root, 'config', 'locales', '**', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:fr, :en]
-    config.i18n.default_locale = :en
+#   config.i18n.available_locales = [:fr, :en]
+    config.i18n.default_locale = :fr
   end
 end

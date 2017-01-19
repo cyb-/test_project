cb7fd18af5b79ac623fb52357ed41fd756d704a7
cyb- <gchateau@student.42.fr>
2017-01-19 02:15:55 +0100

    nice layout and css

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,10 +1,10 @@
 module BootstrapHelper
-	def bootstrap_flash_class_for(flash_type)
-		{
-			success: "alert-success",
-			error: "alert-danger",
-			alert: "alert-warning",
-			notice: "alert-info"
-		}[flash_type.to_sym] || "alert-info"
-	end
+  def bootstrap_flash_class_for(flash_type)
+    {
+      success: "alert-success",
+      error: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info"
+    }[flash_type.to_sym] || "alert-info"
+  end
 end
--- spec/helpers/bootstrap_helper_spec.rb
+++ spec/helpers/bootstrap_helper_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe BootstrapHelper, type: :helper do
 
-	describe "#bootstrap_flash_class_for" do
-		it "return valid Bootstrap class for flash message" do
-			expect(helper.bootstrap_flash_class_for(:success)).to eq("alert-success")
-		end
+  describe "#bootstrap_flash_class_for" do
+    it "return valid Bootstrap class for flash message" do
+      expect(helper.bootstrap_flash_class_for(:success)).to eq("alert-success")
+    end
 
-		it "return default 'alert-info' class" do
-			expect(helper.bootstrap_flash_class_for(:invalid_type)).to eq("alert-info")
-		end
-	end
+    it "return default 'alert-info' class" do
+      expect(helper.bootstrap_flash_class_for(:invalid_type)).to eq("alert-info")
+    end
+  end
 
 end
